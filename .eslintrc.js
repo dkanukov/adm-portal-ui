bd4ae11028f01b978d2162a17ba5a0041150826d
Denis <kanukov.denis@gmail.com>
2023-03-13 03:17:32 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/vue3-essential',
+		'plugin:vue/vue3-recommended',
+		'plugin:vue/base',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'project': true,
+		'tsconfigRootDir': __dirname,
+	},
+	'plugins': [
+		'vue',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'@typescript-eslint/no-floating-promises': 'warn',
+	},
+}
